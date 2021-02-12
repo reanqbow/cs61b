109d90995fd3404324912025f66f97bff1eec3c3
reanqbow <reanqbow@gmail.com>
2021-02-12 12:30:14 -0500

    Modify according to autograder

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,9 +4,9 @@ public class Planet {
     public double xxVel;
     public double yyVel;
     public double mass;
-    String imgFileName;
+    public String imgFileName;
 
-    public static final double G = 6.67e-11;
+    private double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
